2f458d0abf09dee390947f7594685eb6f830e863
Lana W <lanawehbeh12@gmail.com>
2023-10-14 11:34:51 -0400

    Starter File Picker code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <h1>Mongo do be tasty!</h1>
-  );
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,49 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const client = filestack.init("APlg3y5HwS180OK92teucz");
+client.picker().open();
+
+const options = {
+    onUploadDone: updateForm,
+    maxSize: 10 * 1024 * 1024,
+    accept: 'image/*',
+    uploadInBackground: false,
+};
+const picker = client.picker(options);
+
+// Get references to the DOM elements
+
+const form = document.getElementById('pick-form');
+const fileInput = document.getElementById('fileupload');
+const btn = document.getElementById('picker');
+const nameBox = document.getElementById('nameBox');
+const urlBox = document.getElementById('urlBox');
+
+// Add our event listeners
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    picker.open();
+});
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Submitting: ' + fileInput.value);
+});
+
+// Helper to overwrite the field input value
+
+function updateForm (result) {
+    const fileData = result.filesUploaded[0];
+    fileInput.value = fileData.url;
+
+    // Some ugly DOM code to show some data.
+    const name = document.createTextNode('Selected: ' + fileData.filename);
+    const url = document.createElement('a');
+    url.href = fileData.url;
+    url.appendChild(document.createTextNode(fileData.url));
+    nameBox.appendChild(name);
+    urlBox.appendChild(document.createTextNode('Uploaded to: '));
+    urlBox.appendChild(url);
+};
